e8b9168bf09b7eca29333295eda00901b45b8af7
Sandra P <putere.sandra@gmail.com>
2021-04-18 10:13:09 +0300

    finished Section 4 from the course

---
+++ src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <div>{{ task.name }} {{ index }}</div>
+    <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Task.vue",
+  props: ['task', 'index']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,79 +1,54 @@
 <template>
   <q-page padding>
-    <button class="counter-button"
-            @click="counter++">{{ counter }}
-    </button>
-    <input
-      v-model="message"
-      @keyup.esc="clearMessage"
-      @keyup.enter="alertMessage"
-      v-autofocus
-      :class="{ 'error' : message.length > 23 }"
-      ref="messageInput" />
-    <button @click="clearMessage">Clear</button>
-    <div>{{ message.length }}</div>
-    <h5 class="border-grey" v-if="message.length">{{ message }}</h5>
-    <h6 v-else>No message entered</h6>
-    <hr>
-    <p>Uppercase message: {{ messageUppercase }}</p>
-    <p>Lowercase message: {{ message | messageLowercase }}</p>
+    <ul>
+      <task
+        v-for="(task, index) in tasks"
+        :key="task.id"
+        :task="task"
+        :index="index"></task>
+    </ul>
   </q-page>
 </template>
 
 <script>
+import task from 'components/Task.vue'
   export default {
     data() {
       return {
-        message: 'I love Vue.js so hard!',
-        counter: 0
+        tasks: [
+          {
+            id: 1,
+            name: 'Go to shop!',
+            dueDate: '2019/05/12',
+            dueTime: '18:30'
+          },
+          {
+            id:2,
+            name: 'Get bananas!',
+            dueDate: '2019/05/13',
+            dueTime: '14:00'
+          },
+          {
+            id: 3,
+            name: 'Get apples!',
+            dueDate: '2019/05/14',
+            dueTime: '16:00'
+          }
+
+        ]
       }
     },
     methods: {
-      clearMessage() {
-        this.message = ''
-      },
-      alertMessage() {
-        alert(this.message)
+      deleteTask(index) {
+        this.tasks.splice(index, 1)
       }
     },
-    computed: {
-      messageUppercase() {
-        return this.message.toUpperCase()
-      }
-    },
-    //in filters, we don't have access to data properties, we don't have access to methods, we don't have access to computed properties. we only have access to the value
-    filters: {
-      messageLowercase(value) {
-        return value.toLowerCase()
-      }
-    },
-    directives: {
-      autofocus: {
-        inserted(el) {
-          el.focus()
-        }
-      }
-    },
-    mounted() {
-     // console.log(this.$refs)
-      this.$refs.messageInput.className = 'bg-green'
-    },
+    components: {
+      task
+    }
   }
 </script>
 
 <style lang="scss">
-  .counter-button {
-    position: absolute;
-    right: 20px;
-  }
-  .border-grey {
-    border: 1px solid grey;
-  }
-  .error {
-    color: red;
-    background: pink;
-  }
-  input, button {
-    font-size: 23px;
-  }
+
 </style>
